332347d140e240d79b2e3f7f1780e1094e973348
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-01-22 11:42:28 -0300

    Testando a criação do schema tweet

--- src/tweets/entities/tweet.entity.spec.ts
+++ src/tweets/entities/tweet.entity.spec.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
-import { Tweet } from './tweet.entity'
+import mongoose from 'mongoose'
+import { Tweet, TweetSchema } from './tweet.entity'
 
 describe('Tweet tests', () => {
   it('should create a tweet', () => {
@@ -7,8 +8,34 @@ describe('Tweet tests', () => {
       content: 'Hello world',
       screenName: 'Kefflen',
     })
-    
+
     expect(tweet.content).toBe('Hello world')
     expect(tweet.screenName).toBe('Kefflen')
+  })
+})
+
+describe('Tweet Schema tests', () => {
+  let connection: mongoose.Mongoose
+
+  beforeAll(async () => {
+    connection = await mongoose.connect('mongodb://root:root@localhost:27017/tweet-test?authSource=admin')
+  })
+
+  afterAll(async () => {
+    await connection.disconnect()
+  })
+  
+  it('should create a tweet document', async () => {
+    const TweetModel = await connection.model('Tweet', TweetSchema)
+    const tweet = new TweetModel({
+      content: 'Hello world',
+      screenName: 'Kefflen',
+    })
+
+    await tweet.save()
+    
+    const createdTweet = await TweetModel.findById(tweet._id)
+    expect(createdTweet.content).toBe('Hello world')
+    expect(createdTweet.screenName).toBe('Kefflen')
   })
 })
--- src/tweets/entities/tweet.entity.ts
+++ src/tweets/entities/tweet.entity.ts
@@ -1,13 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+
 export type tweetProps = {
   content: string
   screenName: string
 }
 
+@Schema()
 export class Tweet {
+  @Prop({ required: true })
   content: string
+  @Prop({ required: true })
   screenName: string
 
   constructor(private props: tweetProps) {
     Object.assign(this, props)
   }
 }
+
+export const TweetSchema = SchemaFactory.createForClass(Tweet)
